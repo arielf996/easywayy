c17b897cedf95f7c90211350d2f853892f922ba2
arielf996 <37421761+arielf996@users.noreply.github.com>
2018-06-19 01:08:43 +0300

    Add files via upload

---
+++ easyway/jScripts/javascript.js
@@ -0,0 +1,26 @@
+function changeIt(event) {
+    event.target.classList.toggle("liStyle");
+
+
+    document.querySelector("#myList").classList.toggle("ulStyle");
+
+}
+function show() {
+
+    document.querySelector("#div2").classList.toggle("DIV2");
+
+}
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
